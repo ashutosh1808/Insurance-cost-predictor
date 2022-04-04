72f008fb82035d33c3637f25365f0b022600013b
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2022-04-04 19:57:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["MAIL_PASSWORD"]="Tester@4567!!!" #passwd
 app.config["MAIL_USE_TLS"]=True #transport layer security
 app.config["MAIL_USE_SSL"]=False #secure sockets layer
 
-mail=Mail(app)
+mail=Mail(app) #instantiate the Mail class
 #home page
 @app.route("/")
 def home():
@@ -25,19 +25,19 @@ def get():
 	lname=request.form["lname"] #likewise for lname
 	if not(lname.isalpha()) or len(lname)<2:
 	    return render_template("home.html",msg="Last name should contain alphabets only")
-	name=fname+" "+lname
+	name=fname+" "+lname #concat into full name
 	sex=request.form.get("sex") #accept sex
 	hf=int(request.form["hf"])  #height in ft
 	hi=int(request.form["hi"])  #height in inches
 	wt=float(request.form["wt"]) #weight in kg -- > all for the bmi
 	height=0.3048*hf+0.0254*hi #convert ft inches to metres
-	bmi=round((wt/height**2),0) 
+	bmi=round((wt/height**2),0)  #round the value of bmi to 0 places
 	pk=""
 	if bmi<=26:
-		pk="you are at low risk"
+		pk="you are at low risk" 
 	else:
 	   pk="you are at high risk"
-	em=request.form["em"]  #accepting the email
+	em=request.form["em"]  #accepting the email from user
 	age=float(request.form["age"]) #age
 	smk=request.form.get("smk") #smoking or not?
 	with open("ic.model","rb") as f: #using the saved model
@@ -50,4 +50,4 @@ def get():
 	return render_template("home.html",msg="Thanks for using this! You will receive a mail shortly.") #show it on the webpage
 
 if __name__=="__main__":  #run the app
-	app.run(debug=True,use_reloader=True)
+	app.run(debug=True,use_reloader=True)  #it can be debugged as well as reloaded whenever needed
