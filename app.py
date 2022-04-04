bfa17ee9d2daa8f498d44803a0babae563c5f3a0
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2022-04-04 18:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request #import the libraries
 import pickle
 from flask_mail import Mail,Message
 
 
-app=Flask(__name__)
-app.config["MAIL_SERVER"]="smtp.gmail.com"
-app.config["MAIL_PORT"]=587
+app=Flask(__name__)	#initialize the flask app
+app.config["MAIL_SERVER"]="smtp.gmail.com"	#gmail
+app.config["MAIL_PORT"]=587	#port no	
 app.config["MAIL_USERNAME"]="tester.insurepro@gmail.com"
-app.config["MAIL_PASSWORD"]="Tester@4567!!!"
-app.config["MAIL_USE_TLS"]=True
-app.config["MAIL_USE_SSL"]=False
+app.config["MAIL_PASSWORD"]="Tester@4567!!!" #passwd
+app.config["MAIL_USE_TLS"]=True #transport 
+app.config["MAIL_USE_SSL"]=False #
 
 mail=Mail(app)
 
