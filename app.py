f32114ca592055732de0e7042a5314587f32a753
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2022-04-04 19:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,46 +8,46 @@ app.config["MAIL_SERVER"]="smtp.gmail.com"	#gmail
 app.config["MAIL_PORT"]=587	#port no	
 app.config["MAIL_USERNAME"]="tester.insurepro@gmail.com"
 app.config["MAIL_PASSWORD"]="Tester@4567!!!" #passwd
-app.config["MAIL_USE_TLS"]=True #transport 
-app.config["MAIL_USE_SSL"]=False #
+app.config["MAIL_USE_TLS"]=True #transport layer security
+app.config["MAIL_USE_SSL"]=False #secure sockets layer
 
 mail=Mail(app)
-
+#home page
 @app.route("/")
 def home():
 	return render_template("home.html")
-
+#after pressing predict option
 @app.route("/get",methods=["POST"])
 def get():
-	fname=request.form["fname"]
-	if not(fname.isalpha()) or len(fname)<2:
+	fname=request.form["fname"] #accept the fname
+	if not(fname.isalpha()) or len(fname)<2:	#validation for fname
 	    return render_template("home.html",msg="First name should contain alphabets only")
-	lname=request.form["lname"]
+	lname=request.form["lname"] #likewise for lname
 	if not(lname.isalpha()) or len(lname)<2:
 	    return render_template("home.html",msg="Last name should contain alphabets only")
 	name=fname+" "+lname
-	sex=request.form.get("sex")
-	hf=int(request.form["hf"])
-	hi=int(request.form["hi"])
-	wt=float(request.form["wt"])
-	height=0.3048*hf+0.0254*hi
-	bmi=round((wt/height**2),0)
+	sex=request.form.get("sex") #accept sex
+	hf=int(request.form["hf"])  #height in ft
+	hi=int(request.form["hi"])  #height in inches
+	wt=float(request.form["wt"]) #weight in kg -- > all for the bmi
+	height=0.3048*hf+0.0254*hi #convert ft inches to metres
+	bmi=round((wt/height**2),0) 
 	pk=""
 	if bmi<=26:
 		pk="you are at low risk"
 	else:
 	   pk="you are at high risk"
-	em=request.form["em"]
-	age=float(request.form["age"])
-	smk=request.form.get("smk")
-	with open("ic.model","rb") as f:
+	em=request.form["em"]  #accepting the email
+	age=float(request.form["age"]) #age
+	smk=request.form.get("smk") #smoking or not?
+	with open("ic.model","rb") as f: #using the saved model
 		model=pickle.load(f)
-	data=[[bmi,age,smk]]
-	res=model.predict(data);	ans=round(res[0],0)
-	msg=Message("Report for the medical insurance",sender="tester.insurepro@gmail.com",recipients=[em])
+	data=[[bmi,age,smk]] 
+	res=model.predict(data);	ans=round(res[0],0) #prediction
+	msg=Message("Report for the medical insurance",sender="tester.insurepro@gmail.com",recipients=[em]) #the body of mail to be sent
 	msg.body="Hey "+str(name).title()+"\n"+"Thanks for using InsurePro. Your medical insurance cost for this month is Rs."+str(ans)+" Your BMI is "+str(bmi)+" which means "+str(pk)
-	mail.send(msg)
-	return render_template("home.html",msg="Thanks for using this! You will receive a mail shortly.")
+	mail.send(msg) #send the mail
+	return render_template("home.html",msg="Thanks for using this! You will receive a mail shortly.") #show it on the webpage
 
-if __name__=="__main__":
+if __name__=="__main__":  #run the app
 	app.run(debug=True,use_reloader=True)
